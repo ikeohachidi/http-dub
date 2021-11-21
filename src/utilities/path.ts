f3c2f50d4526b8bb5274dfeda97900dbc686c3fc
ikeohachidi <ikeohachidi@gmail.com>
2021-11-21 13:57:14 +0100

    docs: document functions

--- src/utilities/path.ts
+++ src/utilities/path.ts
@@ -1,9 +1,21 @@
 import { OptionArguments } from '../types';
 
+/**
+ *	Removes all unused identifiers from url
+ *	Eg of identifiers /:id 
+ *	@param path the url to be trimmed
+ *	@returns string
+ */
 const removeIdsFromPath = (path: string): string => {
 	return path.replace(/\b\/:\S+?(\/|\b)\b/g, "");
 }
 
+/**
+ *	normalizes paths will identifier values
+ *	@param path url
+ *	@param replacers key/value pair with the keys matching a substring in path and the values the replacement
+ *	@returns 
+ */
 export const pathReplacer = (path: string, replacers?: OptionArguments): string => {
 	if (!replacers) return removeIdsFromPath(path);
 
